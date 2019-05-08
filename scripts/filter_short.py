4788b1899e0e3f4a1f35c3c0475c458949acb9cc
Ethan Chau <echau18@cs.uw.edu>
2019-05-07 21:17:23 -0700

    fix filtering on empty tweets

--- scripts/filter_short.py
+++ scripts/filter_short.py
@@ -1,6 +1,6 @@
 import sys
 
-THRESHOLD = 2
+THRESHOLD = 3 
 buf = []
 acc_length = 0
 
@@ -9,20 +9,28 @@ with open(sys.argv[1], 'r') as raw, \
     for line in raw:
         components = line.strip().split('\t')
 
-        if len(components) >= 7:
-            if acc_length > THRESHOLD:
-                for ln in buf:
-                    out.write(ln)
+        # we need to handle for the edge case where the Tweet body is empty
+        # which occurs when there are > 2 and < 7 components
+        if len(components) > 2:
+            if acc_length >= THRESHOLD:
+                out.write(' '.join(buf))
+                out.write('\n')
+                # for ln in buf:
+                #     out.write(ln)
             buf = []
             acc_length = 0
             index = 6
+            if len(components) < 7:
+                continue
         else:
             index = 0
 
         body = components[index]
         
         acc_length += len(body.split())
-        buf.append(line)
+        buf.append(line.strip())
     if acc_length > THRESHOLD:
-        for ln in buf:
-            out.write(ln)
+        out.write(' '.join(buf))
+        out.write('\n')
+        # for ln in buf:
+        #     out.write(ln)
